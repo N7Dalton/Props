33df6d76a6c7ab5a6e79011da5e672868304c095
Dalton <n7dalton@gmail.com>
2023-04-20 08:25:11 -0500

    oops

---
+++ Properties_Lumpkin_Dalton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Properties_Lumpkin_Dalton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car car1 = new car();
+            car car2 = new car();
+
+            // sets the make and model for both cars
+            car1.Make = "Challenger";
+            car2.Make = "Ford";
+            car1.Model = "Hellcat";
+            car2.Model = "F150";
+
+            // I love console.WriteLine :)
+            Console.WriteLine($"Your first car was a {car1.Make} and is a {car1.Model}? dope.");
+            Console.WriteLine($"Your second car was a{car2.Make} and is a {car2.Model}? dope.");
+        }
+    }
+}
---
+++ Properties_Lumpkin_Dalton/car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties_Lumpkin_Dalton
+{
+    class car
+    {
+		private string model;
+
+		// sets properties for model
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+		// gets and sets for Make
+		public string Make
+		{
+			get; set;
+		}
+	}
+}
